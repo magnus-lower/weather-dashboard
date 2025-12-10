6499b006b3e47c91910facb5b35bb524ef0a58ca
Magnus Løwer <148643539+magnus-lower@users.noreply.github.com>
2025-12-10 11:38:07 +0100

    Streamline WSGI entrypoint

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
-"""WSGI entrypoint for running the Flask application."""
-
 from app import create_app
 
-
 application = create_app()
-
 
 if __name__ == "__main__":
     application.run(host="127.0.0.1", port=5000)
